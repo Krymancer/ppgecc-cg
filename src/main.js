b570aa751b9f771a3003b485b56198d4f4b85f92
Júnior Nascimento <junior.nascm@gmail.com>
2023-11-27 16:49:40 -0300

    fonting rendering with particles testing

--- src/main.js
+++ src/main.js
@@ -1,26 +1,58 @@
 import * as THREE from 'three';
+import { FontLoader } from 'three/addons/loaders/FontLoader.js';
+import { TextGeometry } from 'three/addons/geometries/TextGeometry.js';
+import typefaceData from "@compai/font-recursive/data/typefaces/normal-400.json";
 
+// Set up the scene
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 const renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
-
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
 
 camera.position.z = 5;
 
-function animate() {
-	requestAnimationFrame( animate );
+const font = new FontLoader().parse(typefaceData);
 
-	cube.rotation.x += 0.01;
-	cube.rotation.y += 0.01;
+const textGeometry = new TextGeometry('P', {
+	font: font,
+	size: 1, // Adjust the size as needed
+	height: 0.1, // Extrusion depth
+	curveSegments: 12,
+	bevelEnabled: false,
+});
 
-	renderer.render( scene, camera );
+const textMaterial = new THREE.MeshBasicMaterial({ color: 0xffffff });
+
+const textMesh = new THREE.Mesh(textGeometry, textMaterial);
+
+// Position and add the text mesh to the scene
+textMesh.position.set(0, 0, 0);
+scene.add(textMesh);
+
+const particleCount = 1000;
+const particles = new THREE.Group();
+
+const positions = textGeometry.attributes.position;
+
+for (let i = 0; i < particleCount; i++) {
+  const randomIndex = Math.floor(Math.random() * positions.count);
+  const randomVertex = new THREE.Vector3().fromBufferAttribute(positions, randomIndex);
+
+  // Add particles at the random vertices
+  const particle = new THREE.Mesh(new THREE.SphereGeometry(0.02), new THREE.MeshBasicMaterial({ color: 0xffffff }));
+  particle.position.copy(randomVertex);
+  particles.add(particle);
 }
 
+scene.add(particles);
+
+function animate() {
+  requestAnimationFrame(animate);
+
+  // Render the scene
+  renderer.render(scene, camera);
+}
+
+// Start the animation loop
 animate();
