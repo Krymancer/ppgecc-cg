9fd5249f6f2ad9258dfcc109c0caf36708890c4a
ciano123 <casinho.555@gmail.com>
2023-12-03 21:53:39 -0300

    Particle dispersion with mouse movement

--- src/main.js
+++ src/main.js
@@ -8,12 +8,8 @@ const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 const renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.shadowMap.enabled = true;
-
-// Otimizações no Renderizador
-const pixelRatio = window.devicePixelRatio || 1;
-renderer.setPixelRatio(pixelRatio);
-const newWidth = window.innerWidth ; // Tamanho reduzido pela metade
-const newHeight = window.innerHeight ; // Tamanho reduzido pela metade
+const newWidth = window.innerWidth; 
+const newHeight = window.innerHeight;
 renderer.setSize(newWidth, newHeight);
 document.body.appendChild(renderer.domElement);
 const controls = new OrbitControls(camera, renderer.domElement);
